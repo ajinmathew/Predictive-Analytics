3d4eb21336894acd96d18c990861e996e4b0e538
ajinmathew <mathewajin10@gmail.com>
2021-04-04 23:07:33 +0530

    eda created-2

---
+++ eda.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns #visualisation
+import matplotlib.pyplot as plt #visualisation
+#%matplotlib inline 
+sns.set(color_codes=True)
+df = pd.read_csv(“data.csv”)
+# To display the top 5 rows
+df.head(5)
